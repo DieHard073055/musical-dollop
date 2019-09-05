6b822096660562a7463fdd88d87440244888388c
DieHard073055 <eshanshafeeq073055@gmail.com>
2019-09-05 18:17:10 +0800

    add get_duplicate_tracks and find_common_tracks
    - add tests and code for the tests

--- src/itunes_playlist_parser.py
+++ src/itunes_playlist_parser.py
@@ -1,13 +1,43 @@
 import json
 import plistlib
+from pathlib import Path
+from typing import List, Dict, Any
 
 
-def json_serializable(o):
+def json_serializable(o: Any) -> str:
     return str(o)
 
-def parse_file(file):
-    playlist = None
+
+def parse_file(file: Path) -> Dict[str, Any]:
+    playlist = dict()
     with open(file, 'rb') as itunes_playlist_file:
         playlist = itunes_playlist_file.read()
         playlist = plistlib.loads(playlist)
     return playlist
+
+
+def get_duplicate_tracks(tracks: List[Dict[str, Any]]) -> List[List[Dict[str, Any]]]:
+    track_duplicates_counter = dict()
+    duplicates = []
+    for track_id, track in tracks.items():
+        if track["Name"] not in track_duplicates_counter.keys():
+            track_duplicates_counter[track["Name"]] = []
+        track_duplicates_counter[track["Name"]].append(track_id)
+    duplicates = [track_ids for track_name, track_ids in track_duplicates_counter.items() if len(track_ids) > 1]
+
+    return duplicates
+
+
+def get_tracks(playlist: List[Dict[str, Any]]) -> List[str]:
+    return [track["Name"] for track_id, track in playlist.items()]
+
+
+def find_common_tracks(playlist_1, playlist_2):
+    tracks_pl_1 = get_tracks(playlist_1)
+    tracks_pl_2 = get_tracks(playlist_2)
+
+    return list(set(tracks_pl_1).intersection(tracks_pl_2))
+
+
+
+
--- tests/test_itunes_playlist_parser.py
+++ tests/test_itunes_playlist_parser.py
@@ -5,9 +5,9 @@ import os
 from pathlib import Path
 
 """ TODO:
-- [ ] find duplicates in the playlist
-- [ ] find common tracks across a list of given playlist
-- [ ] plot the 
+- [x] find duplicates in the playlist
+- [x] find common tracks across a list of given playlist
+- [ ] plot common statistics
 """
 TEST_DIR_NAME='data'
 TEST_DIR_PATH=Path(os.path.join(os.path.dirname(__file__), TEST_DIR_NAME))
@@ -19,22 +19,46 @@ def read_results_file(results_file):
     with open(results_file, "r") as rfile:
         return rfile.read()
 
-@pytest.mark.parametrize("itunes_playlist_files", [
-    ("maya.xml"),
-    ('mymusic.xml'),
-    ('pl1.xml'), 
-    ('pl2.xml')
+@pytest.mark.parametrize("playlist_file", [
+        ('maya.xml'),
+        ('mymusic.xml'),
+        ('pl1.xml'),
+        ('pl2.xml')
 ])
-def test_parse_file(itunes_playlist_files):
-    items = itunes_playlist_parser.parse_file(os.path.join(TEST_DIR_PATH, itunes_playlist_files))
-    file_path = os.path.join(TEST_DIR_PATH, f"{itunes_playlist_files}_parsed_results.txt")
+def test_parse_file(playlist_file):
+    items = itunes_playlist_parser.parse_file(os.path.join(TEST_DIR_PATH, playlist_file))
+    file_path = os.path.join(TEST_DIR_PATH, f"{playlist_file}_parsed_results.txt")
     assert read_results_file(file_path) == json.dumps(items, indent=4, default=json_serializable)
 
-def test_duplicates_in_playlist():
-    raise NotImplementedError()
+@pytest.mark.parametrize("playlist_file, nof_duplicates", [
+        ('maya.xml', 0),
+        ('mymusic.xml', 238),
+        ('pl1.xml', 1),
+        ('pl2.xml', 3)
+])
+def test_duplicates_in_playlist(playlist_file, nof_duplicates):
+    items = itunes_playlist_parser.parse_file(os.path.join(TEST_DIR_PATH, playlist_file))
+    tracks = items["Tracks"]
 
-def test_find_common_tracks_across_playlists():
-    raise NotImplementedError()
+    # calculate duplicates
+    track_duplicates_counter = dict()
+    duplicates = itunes_playlist_parser.get_duplicate_tracks(tracks)
+    assert len(duplicates) == nof_duplicates, "should have the exact amount of duplicates we provided"
+
+@pytest.mark.parametrize("playlist_file_1, playlist_file_2, expected_nof_common_tracks",[
+        ('maya.xml', 'mymusic.xml', 67),
+        ('pl1.xml', 'pl2.xml', 15)
+])
+def test_find_common_tracks_across_playlists(playlist_file_1, playlist_file_2, expected_nof_common_tracks):
+   playlist_1 = itunes_playlist_parser.parse_file(os.path.join(TEST_DIR_PATH, playlist_file_1))["Tracks"]
+   playlist_2 = itunes_playlist_parser.parse_file(os.path.join(TEST_DIR_PATH, playlist_file_2))["Tracks"]
+
+   common_tracks = itunes_playlist_parser.find_common_tracks(playlist_1, playlist_2)
+
+   assert (
+           len(common_tracks) == expected_nof_common_tracks
+    ), "should have the expected common tracks if all the commons tracks were found"
+
 
 def test_plot_stats_about_playlists():
     raise NotImplementedError()
